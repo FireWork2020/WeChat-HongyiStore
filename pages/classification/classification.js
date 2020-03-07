397a63e3c91fef0c8b10ff7a61b555d7bc9201e5
FireWork2020 <guoyuxiang13123@163.com>
2020-03-07 23:11:22 +0800

    ordinary commit

--- pages/classification/classification.js
+++ pages/classification/classification.js
@@ -6,24 +6,53 @@ Page({
    */
   data: {
     onLoadStatus:true,
+    curNav:0,
+    // menus:[
+    //   {
+    //     name:"衣服",
+    //     children:[
+    //       {
+    //         name:"上衣"
+    //       },
+    //       {
+    //         name:"下衣"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     name: "化妆品"
+    //   },
+    //   {
+    //     name: "食物"
+    //   }
+    // ]
     menus:[]
   },
-
+  showSelectedChildMenu: function (e) {
+    let index = parseInt(e.target.dataset.index);
+    console.log(index)
+    this.setData({
+      curNav: index
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var _this = this;
     wx.request({
-      //url:'http://localhost:8080/menu/allmenus',
-      url:'http://localhost:8080/goods/goodsByParentMenu',
+      url:'http://localhost:8080/menu/allmenus',
       data:{
         menuId:1
       },
       success(res){
-        //console.log(res);
-        //this.menus = res;
+        console.log(res);
+        _this.setData({
+          menus: res.data
+        });
       }
-    })
+    });
+    
   },
 
   /**
